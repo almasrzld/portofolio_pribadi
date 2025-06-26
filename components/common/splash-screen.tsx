5988abb89d1b9287579542085c297e87ecb5dded
almasrzld <almasrzld@gmail.com>
2025-06-26 07:12:20 +0700

    feat: Update splash screen to dynamically adjust text offset based on window size

--- components/common/splash-screen.tsx
+++ components/common/splash-screen.tsx
@@ -6,25 +6,47 @@ import { motion } from "framer-motion";
 const SplashScreen = () => {
   const TEXT_ITEMS = ["WELCOME", "TO", "MY", "PERSONAL", "PORTFOLIO"];
   const [centered, setCentered] = useState(false);
+  const [offsetValue, setOffsetValue] = useState(255);
 
   useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width < 640) {
+        setOffsetValue(10); // mobile
+      } else if (width < 768) {
+        setOffsetValue(30); // tablet kecil
+      } else if (width < 1024) {
+        setOffsetValue(50); // tablet besar
+      } else if (width < 1440) {
+        setOffsetValue(200); // laptop
+      } else {
+        setOffsetValue(250); // desktop
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
     const timer = setTimeout(() => {
       setCentered(true);
     }, 1500);
 
-    return () => clearTimeout(timer);
+    return () => {
+      clearTimeout(timer);
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   return (
     <div className="fixed inset-0 bg-background text-primary flex items-center justify-between z-[9999]">
       {TEXT_ITEMS.map((text, index) => {
-        const offset = (index - TEXT_ITEMS.length / 2 + 0.5) * 255;
+        const offset = (index - TEXT_ITEMS.length / 2 + 0.5) * offsetValue;
         const isBold = text === "MY" || text === "PERSONAL";
 
         return (
           <motion.h1
             key={index}
-            className={`text-4xl ${
+            className={`text-2xl md:text-4xl ${
               isBold ? "font-bold" : "font-normal"
             } font-instrumentserif`}
             initial={{ x: 0, opacity: 0 }}
